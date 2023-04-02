3fd85a242172544e44e8bfe7d2000c2a020efd59
ali-ismaeel564 <aliismaeel564@gmail.com>
2023-04-02 18:04:31 +0500

    server file updated

--- models/userRepModel.js
+++ models/userRepModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const userRepSchema = mongoose.Schema(
 {
-  userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
   videoID: { type: mongoose.Schema.Types.ObjectId, ref: 'Video', required: true },
   liftType: {type: String, required: true},
   attemptedReps: {type: Number, required: true},
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const userModel = require("./models/userModel");
 const userRepsModel = require("./models/userRepModel");
-const userLiftModel = require("./models/userLiftModel");
 const mongoose = require("mongoose");
 const app = express();
 app.use(cors());
@@ -11,20 +10,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-  // const curr = new Date("2023-03-04T12:00:00.000Z");
-  // const first = curr.getDate() - curr.getDay();
-  // console.log(curr.getDate());
-  // console.log(curr.getDay());
-  // const last = first + 6;
-
-  // var firstday = new Date(curr.setDate(first)).toUTCString();
-  // var lastday = new Date(curr.setDate(last)).toUTCString();
-
-  // res
-  //   .status(200)
-  //   .send({ currentDate: curr, startDate: firstday, endDate: lastday });
-
-  res.status(200).send("fitxAPI is live now...")
+  res.status(200).send("fitxAPI is live now...");
 });
 
 app.post("/api/user", async (req, res) => {
@@ -44,7 +30,7 @@ app.post("/api/user", async (req, res) => {
           .send({ message: `'${emailExist["email"]}' is already exist` });
       } else {
         const user = await userModel.create(req.body);
-        res.status(200).send(user);
+        res.status(201).send({ data: user });
       }
     }
   } catch (error) {
@@ -55,9 +41,9 @@ app.post("/api/user", async (req, res) => {
 app.post("/api/userReps", async (req, res) => {
   try {
     const userReps = await userRepsModel.create(req.body);
-    res.status(200).send(userReps);
+    res.status(201).send({ status: "success", data: { userReps } });
   } catch (error) {
-    res.status(500).send({ message: error.message });
+    res.status(400).send({ status: "error", message: error.message });
   }
 });
 
@@ -66,8 +52,13 @@ app.get("/api/userLift/:userID", async (req, res) => {
     const { userID } = req.params;
     const { liftType } = req.query;
     const { date } = req.query;
+    if (!date) {
+      return res.status(400).send({
+        message:
+          "Date is required in the format yyyy-mm-dd (2023-05-21T12:00:00.000Z)",
+      });
+    }
     const curr = new Date(date);
-    // const curr = new Date("2023-05-21T12:00:00.000Z");
     const first = curr.getDate() - curr.getDay();
     const last = first + 6;
 
@@ -83,17 +74,76 @@ app.get("/api/userLift/:userID", async (req, res) => {
       },
     });
 
+    if (!userReps || userReps.length === 0) {
+      return res.status(404).send({ message: "User reps not found" });
+    }
+
     // calculate the sum of attempted reps
     const sumAttemptedReps = userReps.reduce((total, userRep) => {
       return total + userRep.attemptedReps;
     }, 0);
 
-    console.log(
-      `Total attempted reps for user ${userID}, lift type ${liftType}, and week starting from ${firstday} are ${sumAttemptedReps}`
-    );
-    res.status(200).send({"total_attemtedReps": sumAttemptedReps});
+    // console.log(
+    //   `Total attempted reps for user ${userID}, lift type ${liftType}, and week starting from ${firstday} are ${sumAttemptedReps}`
+    // );
+    res.status(200).send({
+      status: "success",
+      data: { userID: userID, total_attemtedReps: sumAttemptedReps },
+    });
   } catch (error) {
-    res.status(505).send({ message: error.message });
+    res.status(505).send({ status: "error", message: error.message });
+  }
+});
+
+app.get("/api/leaderboards", async (req, res) => {
+  try {
+    const { liftType, limit } = req.query;
+    const userReps = await userRepsModel.aggregate([
+      {
+        $lookup: {
+          from: "users",
+          localField: "userID",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $match: { liftType: liftType },
+      },
+      {
+        $unwind: "$user",
+      },
+      {
+        $group: {
+          _id: "$userID",
+          totalAttemptedReps: { $sum: "$attemptedReps" },
+          weight: { $first: "$user.weight_kg" },
+          liftType: { $first: "$liftType" },
+        },
+      },
+      {
+        $project: {
+          userID: "$_id",
+          weight: 1,
+          liftType: 1,
+          totalAttemptedReps: 1,
+          _id: 0,
+        },
+      },
+      {
+        $sort: { weight: -1, totalAttemptedReps: -1 },
+      },
+      {
+        $limit: parseInt(limit) || 1000,
+      },
+    ]);
+    if (userReps.length > 0) {
+      res.status(200).send({ status: "success", data: userReps });
+    } else {
+      res.status(404).send({ status: "error", message: "No user Reps found" });
+    }
+  } catch (error) {
+    res.status(505).send({ status: "error", message: error.message });
   }
 });
 
