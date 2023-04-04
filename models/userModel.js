a02a94413abe64011400f36295042b1d70e6793f
ali-ismaeel564 <aliismaeel564@gmail.com>
2023-04-04 21:08:05 +0500

    changings again

--- models/userModel.js
+++ models/userModel.js
@@ -1,19 +1,15 @@
 const mongoose = require("mongoose");
 const userSchema = mongoose.Schema(
   {
-    userID: {type: String},
+    userID: { type: String, required: [true, "Please enter userID"] },
     email: {
       type: String,
       required: [true, "Please enter your email"],
     },
-    firstName: {
-      type: String,
-      required: [true, "Please enter your firstname"]
-    },
-    lastName: { type: String,
-      required: [true, "Please enter your lastname"] },
+    firstName: { type: String },
+    lastName: { type: String },
     height_cm: { type: Number },
-    weight_kg: { type: Number, required: [true, "weight is required"] },
+    weight_kg: { type: Number },
   },
   {
     timestamps: true,
